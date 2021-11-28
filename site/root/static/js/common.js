210226454c22f3b850421776d890c6285e785e53
Christian <61216427+s-christian@users.noreply.github.com>
2021-11-28 18:01:46 -0500

    Music setting is now correctly respected, and intro voice only plays on the front page

--- site/root/static/js/common.js
+++ site/root/static/js/common.js
@@ -1,62 +1,72 @@
-/*
-	BUGS:
-		- If user options are set to mute music, music still plays when loading the page.
-*/
+// *** Functions for options
+function disableScanlines() {
+	scanlinesStylesheet = document.getElementById("scanlinesStylesheet").disabled = true
+}
+function enableScanlines() {
+	scanlinesStylesheet = document.getElementById("scanlinesStylesheet").disabled = false
+}
 
+let fadeInInterval
+const fadeStepMs = 200
+function stopAudio(audio) {
+	clearInterval(fadeInInterval)
+	audio.pause()
+	audio.currentTime = 0
+}
+function fadeInAudio(audio, fadeDelay) {
+	stopAudio(audio)
+	audio.volume = 0
+	audio.play()
+
+	fadeInInterval = setInterval(() => {
+		// Start fading in half a second before the intro voice ends
+		if ((audio.currentTime >= fadeDelay) && (audio.volume < 1.0)) {
+			audio.volume += 0.1
+		}
+		// When volume reaches 1 (max) stop the interval
+		if (audio.volume >= 0.99) { // it never reaches 1.0 for some reason, just 0.999...
+			clearInterval(fadeInInterval)
+		}
+	}, fadeStepMs)
+}
+
+// *** Main content
 document.addEventListener("DOMContentLoaded", () => {
-	/* --- Functions for options --- */
-	function stopScanlines() {
-		const scanlinesStylesheet = document.getElementById("scanlinesStylesheet")
-		scanlinesStylesheet.disabled = true
-	}
-	function startScanlines() {
-		const scanlinesStylesheet = document.getElementById("scanlinesStylesheet")
-		scanlinesStylesheet.disabled = false
-	}
+	/* --- Apply user options, read "options" cookie --- */
+	let playAudio = true
+	let options = {}
+	if (getCookie("options")) {
+		options = JSON.parse(getCookie("options"))
 
-	let fadeInInterval
-	function stopAudio(audio) {
-		clearInterval(fadeInInterval)
-		audio.pause()
-		audio.currentTime = 0
-	}
-	function fadeInAudio(audio, fadeStart) {
-		stopAudio(audio)
-		audio.volume = 0
-		audio.play()
+		if (options.scanlines === false) { // default is "true"
+			document.getElementById("scanlinesRadioOff").setAttribute("checked", true)
+			disableScanlines()
+		}
 
-		fadeInInterval = setInterval(() => {
-			// Start fading in half a second before the intro voice ends
-			if ((audio.currentTime >= fadeStart) && (audio.volume < 1.0)) {
-				audio.volume += 0.1
-			}
-			// When volume reaches 1 (max) stop the interval
-			if (audio.volume >= 0.99999) { // it never reaches 1.0 for some reason, just 0.999...
-				clearInterval(fadeInInterval)
-			}
-		}, 200)
-	}
-	function startMusic() {
-		fadeInAudio(bgm, 0)
-	}
-
-	// Used for testing, unused, can likely be deleted
-	async function sleep(time) {
-		await new Promise((r) => setTimeout(r, time))
+		if (options.bgm === false) { // default is "true"
+			document.getElementById("bgmRadioOff").setAttribute("checked", true)
+			playAudio = false
+		}
 	}
 
 
-	/* --- Play intro voice and fade in background music --- */
-	const introVoice = document.getElementById("introVoice")
-	introVoice.play()
-
+	/* --- Handle audio on page load --- */
 	const bgm = document.getElementById("bgm")
-
-	// Must wait for the audio's metadata to load before we can access its duration,
-	// otherwise we get NaN
-	introVoice.addEventListener("loadedmetadata", () => {
-		fadeInAudio(bgm, introVoice.duration - 0.5)
-	})
+	let introVoice
+	if (playAudio) {
+		// play intro voice if on front page (the only place page it's on)
+		introVoice = document.getElementById("introVoice")
+		if (introVoice) {
+			introVoice.play()
+			// Must wait for the audio's metadata to load before we can access
+			// its duration, otherwise it's NaN
+			introVoice.addEventListener("loadedmetadata", () => {
+				fadeInAudio(bgm, introVoice.duration - 0.5)
+			})
+		} else {
+			fadeInAudio(bgm, 0)
+		}
+	}
 
 
 	/* --- Toggle options box visibility --- */
@@ -75,50 +85,34 @@ document.addEventListener("DOMContentLoaded", () => {
 	})
 
 
-	/* --- Apply user options, read "options" cookie --- */
-	let options = {}
-	if (getCookie("options") !== "") {
-		options = JSON.parse(getCookie("options"))
-
-		if (options.scanlines === false) { // "true" is the default
-			document.getElementById("scanlinesRadioOff").setAttribute("checked", true)
-			stopScanlines()
-		}
-
-		if (options.bgm === false) { // "true" is the default
-			document.getElementById("bgmRadioOff").setAttribute("checked", true)
-			// TODO: Figure out why music still plays regardless upon startup
-			bgm.addEventListener("load", () => {
-				stopAudio(bgm)
-			})
-		}
-	}
-
-
 	/* --- Handle newly-set options, set "options" cookie --- */
 	document.addEventListener("click", (event) => {
 		if (event.target.tagName !== "INPUT") return // we only care about when users click on the radio inputs
+
 		// Otherwise, adjust the options and set the "options" cookie
 		switch (event.target.name) {
 			case "scanlinesRadio":
 				if (event.target.value === "on") {
 					options.scanlines = true
-					startScanlines()
+					enableScanlines()
 				} else {
 					options.scanlines = false
-					stopScanlines()
+					disableScanlines()
 				}
 				break
 			case "bgmRadio":
 				if (event.target.value === "on") {
 					options.bgm = true
-					startMusic()
+					// only play if paused (don't restart already-playing audio)
+					bgm.paused && fadeInAudio(bgm, 0)
 				} else {
 					options.bgm = false
-					stopAudio(bgm)
+					// only stop if currently playing (don't stop already-stopped audio)
+					!bgm.paused && stopAudio(bgm)
 				}
 				break
 		}
+
 		setCookie("options", JSON.stringify(options))
 	})
 })
